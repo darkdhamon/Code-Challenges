19e3c75155f7dca80db6483d79b369b1846760c9
Bronze Brown <dhamon.nightstar@gmail.com>
2022-10-27 14:53:39 -0400

    GM Financial Code Challenge Completed
     - Code needed to be compatible with C# 9.0 as the code was pasted into HireVue Website after I wrote it.

---
+++ 20221027 - GMFinancial Tech Challenge/GMFinancial Tech Challenge/GM Financial Tech Challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace GM_Financial_Tech_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var reader = new StreamReader(Console.OpenStandardInput());
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    var lineParts = line!.Split(',');
+                    if (lineParts.Length == 2)
+                    {
+                        var originalText = lineParts[0];
+                        var charactersToRemove = lineParts[1].Trim().ToCharArray();
+                        var finalText = originalText;
+                        foreach (var c in charactersToRemove) finalText = finalText.Replace($"{c}", string.Empty);
+                        Console.WriteLine(finalText);
+                    }
+                    else
+                    {
+                        // Number of Parameter conditions not specified in requirements
+                    }
+                }
+                else
+                {
+                    // Empty Line Conditions not specified in requirements
+                }
+            }
+        }
+    }
+}
---
+++ 20221027 - GMFinancial Tech Challenge/GMFinancial Tech Challenge/GM Financial Tech Challenge/requirements.md
@@ -0,0 +1,30 @@
+﻿# Challenge 
+## Remove Characters 
+### Programming challenge description: 
+Write a program to remove specific 
+characters from a string. 
+### Input: 
+Your program should read lines of text 
+from standard input. Each line of input will 
+be a string followed by a comma followed 
+by characters that need to be scrubbed. 
+### Output: 
+Print to standard output the scrubbed 
+strings, one per line. Trim out any 
+leading/traiüng whitespaces if they occur. 
+## Test 1 
+Test Input:
+
+`how are you, abc`
+
+Expected Output:
+
+`how re you`
+## Test 2 
+Test Input:
+
+`hello world, def `
+
+Expected Output:
+
+`hllo worl`
